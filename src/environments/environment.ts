a44c8061ea8deb910a9848dabd2ca409fd66529e
Angel Villarreal Bazan <122506691+Nevatrix@users.noreply.github.com>
2026-05-12 21:22:15 -0500

    config(env): set production API base URL to smart-api.nutriproject.xyz

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:8080/api/v1',
+  apiBaseUrl: 'https://smart-api.nutriproject.xyz/api/v1',
   iamEndpointPath: '/users',
   nutritionLogEndpointPath: '/nutrition-log',
   foodSearchEndpointPath: '/foods',
